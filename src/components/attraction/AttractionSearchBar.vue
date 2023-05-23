0aa0ed55a0d94a8b147ee22a86aafe01634a5b0c
marugy <gyulife7301@naver.com>
2023-05-24 00:44:44 +0900

    feat : 여행지 검색 후 마커, 인포 표시

--- src/components/attraction/AttractionSearchBar.vue
+++ src/components/attraction/AttractionSearchBar.vue
@@ -1,6 +1,5 @@
 <template>
   <b-row class="mt-4 mb-4 text-center">
-
     <b-col class="sm-3">
       <b-form-select v-model="sidoCode" :options="sidos" @change="gugunList"></b-form-select>
     </b-col>
@@ -20,8 +19,11 @@
 <script>
 import { mapState, mapActions } from "vuex";
 const attractionStore = "attractionStore";
+
 var makerList = [];
-var ovList= [];
+var ovList = [];
+var map = null;
+
 export default {
   name: "AttractionSearchBar",
   data() {
@@ -54,7 +56,11 @@ export default {
       if (this.sidoCode) this.getGugun(this.sidoCode);
     },
     async searchAttraction() {
-      await this.getAttractionList({ sidoCode: this.sidoCode, gugunCode: this.gugunCode, contentTypeId: this.contentTypeId });
+      await this.getAttractionList({
+        sidoCode: this.sidoCode,
+        gugunCode: this.gugunCode,
+        contentTypeId: this.contentTypeId,
+      });
       this.cleanMaker();
       this.loadMarker();
     },
@@ -71,87 +77,120 @@ export default {
         center: new window.kakao.maps.LatLng(37.500613, 127.036431), // 지도의 중심좌표
         level: 5, // 지도의 확대 레벨
       };
-      this.map = new window.kakao.maps.Map(container, options);
+      map = new window.kakao.maps.Map(container, options);
       // this.loadMarker();
     },
     // 마커 찍기
     loadMarker() {
-      
       makerList = [];
-      for (let k = 0; k < this.attractions.length; k++){
+
+      for (let k = 0; k < this.attractions.length; k++) {
         let marker = new window.kakao.maps.Marker({
-          map: this.map,
+          map: map,
           position: new window.kakao.maps.LatLng(
             this.attractions[k].latitude,
             this.attractions[k].longitude
           ),
-          content: this.attractions[k].title
-        })
+          content: this.attractions[k].title,
+        });
+
         // 인포 윈도우 띄우기
-        let content= '<div class="wrap">' + 
-    '    <div class="info  shadow ">' + 
-    '        <div class="bg-primary ">' +
-    '        		<div class="text-light font-weight-bold p-1 d-flex justify-content-between">' + 
-                  this.attractions[k].title +
-    '         		<div class="far fa-times-circle fa-lg" onclick="closeOverlay('+k+')">닫기</div>' + 
-    '        		</div>'+
-    '		   </div>' + 
-    '        <div class="body">' + 
-    '            <div class="img">' +
-    '                <img src="'+ this.attractions[k].firstImage+'" width="73" height="70">' +
-    '           </div>' + 
-    '            <div class="desc">' + 
-    '                <div class="ellipsis">'+ this.attractions[k].addr1+'</div>' + 
-    '                <div class="jibun ellipsis">(우)'+ this.attractions[k].zipcode+'</div>' +
-    '                <div class="d-flex justify-content-end">';
-    
-    // if(user != "" && positions[i].like == false) {
-    // 	content += '<button id="btn-scrap" class="btn btn-outline-warning d-flex justify-content-center p-1 m-2" type="button" onclick="like('+i+')"> like </button>';
-    // } else if(user != "" && positions[i].like == true) {
-    // 	marker.setImage(markerImage);
-    // 	content += '<button id="btn-scrap" class="btn btn-outline-danger d-flex justify-content-center p-1 m-2" type="button" onclick="like('+i+')"> dislike </button>';
-    // }
-      
-      content +=     
-        '                </div>' + 
-        '            </div>' + 
-        '        </div>' + 
-        '    </div>' +    
-        '</div>';
-        // console.log(content);
+        let content =
+          // `<div class="wrap">
+          //       <div class="info  shadow >
+          //           <div class="bg-primary ">
+          //           		<div class="text-light font-weight-bold p-1 d-flex justify-content-between">
+          //   ${this.attractions[k].title}
+          //            		<div class="far fa-times-circle fa-lg id="close${k}"
+          //   onclick="">닫기</div>
+          //           		</div>
+          //   		   </div>
+          //           <div class="body bg-light">
+          //               <div class="img">
+          //                   <img src="
+          //   ${this.attractions[k].firstImage}
+          //   " width="73" height="70">
+          //              </div>
+          //               <div class="desc">
+          //                   <div class="ellipsis">
+          //   ${this.attractions[k].addr1}
+          //   </div>
+          //                   <div class="d-flex justify-content-end">
+          //                   </div>
+          //               </div>
+          //           </div>
+          //       </div>
+          //   </div>";`;
+
+          // window.kakao.maps.event.addListener(marker, "click", () => {
+          //   overlay.setMap(this.map);
+          // });
+
+          // window.document.getElementById(`close${k}`).onclick = function () {
+          //   ovList[k].setMap(null);
+          // };
+          // console.log(`close${k}`);
+          // console.log(document.getElementsByClassName(`close${k}`)[0]);
+          // document.getElementsByClassName(`close${k}`)[0];
+          // window.kakao.maps.event.addListener(marker, "mouseover", () => {
+          //   overlay.setMap(this.map);
+          // });
+
+          // window.kakao.maps.event.addListener(marker, "mouseout", () => {
+          //   overlay.setMap(null);
+          // });
+
+          // let closeBtn = document.createElement("div");
+          // closeBtn.appendChild(document.createTextNode("닫기"));
+          // closeBtn.onclick = () => {
+          //   ovList[k].setMap(null);
+          // };
+
+          document.createElement("div");
+        content.innerHTML = this.attractions[k].title;
+        content.style.cssText = "background: white; border: 1px solid black";
 
         let overlay = new window.kakao.maps.CustomOverlay({
           content: content,
-          position: marker.getPosition()       
+          position: marker.getPosition(),
         });
 
-        window.kakao.maps.event.addListener(marker, 'click', () => {
-          overlay.setMap(this.map);
-        });
-        window.kakao.maps.event.addListener(marker, 'mouseover', () => {
-          overlay.setMap(this.map);
-        });
-        window.kakao.maps.event.addListener(marker, 'mouseout', () => {
+        var closeBtn = document.createElement("button");
+        closeBtn.innerHTML = "닫기";
+        closeBtn.onclick = function () {
           overlay.setMap(null);
+        };
+
+        content.appendChild(closeBtn);
+        overlay.setContent(content);
+
+        window.kakao.maps.event.addListener(marker, "click", function () {
+          overlay.setMap(map);
         });
+
         ovList[k] = overlay;
 
         makerList[k] = marker;
+
         if (k == 0) {
-          this.map.panTo(new window.kakao.maps.LatLng(this.attractions[k].latitude, this.attractions[k].longitude));
+          map.panTo(
+            new window.kakao.maps.LatLng(
+              this.attractions[k].latitude,
+              this.attractions[k].longitude
+            )
+          );
         }
-        
       }
     },
-    cleanMaker() {
-      for (let k = 0; k < makerList.length; k++){
 
+    // closeOverlay(num) {
+    //   ovList[num].setMap(null);
+    // },
+    cleanMaker() {
+      for (let k = 0; k < makerList.length; k++) {
         makerList[k].setMap(null);
       }
     },
-    closeOverlay(i) {
-      ovList[i].setMap(null);     
-    }   
   },
 };
 </script>
