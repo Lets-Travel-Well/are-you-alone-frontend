d465835d03584bafbd40826a43c3ace13acfeea4
marugy <gyulife7301@naver.com>
2023-05-24 12:08:54 +0900

    refactor : attraction api 수정

--- src/api/attraction.js
+++ src/api/attraction.js
@@ -1,23 +1,17 @@
-import axios from "axios";
-
-const api = axios.create({
-    baseURL: "http://localhost:80/api/attraction-management",
-    headers: {
-      "Content-Type": "application/json;charset=utf-8",
-    },
-  });
-
-// function getAttraction(sidoCode, gugunCode, contentTypeId, success, fail){
-//     api.get(`/attraction?sidoCode=${sidoCode}&gugunCode=${gugunCode}&contentTypeId=${contentTypeId}`).then(success).catch(fail);
-// }
+import { attractionInstance } from './lib';
 
 function listSido(success, fail) {
+  let api = attractionInstance();
     api.get(`/sido`).then(success).catch(fail);
 }
 function listGugun(sidoCode, success, fail) {
+  let api = attractionInstance();
+
     api.get(`/gugun/${sidoCode}`).then(success).catch(fail)
 }
 async function listAttraction(sidoCode, gugunCode, contentTypeId, success, fail) {
+  let api = attractionInstance();
+
     await api.get(`/attraction?sidoCode=${sidoCode}&gugunCode=${gugunCode}&contentTypeId=${contentTypeId}`).then(success).catch(fail)
 }
 export { listSido, listGugun, listAttraction };
